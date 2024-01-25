d523a02682ab2057239a9fc869ee8043babd26ad
Ernest Terjyan <135427178+ernestterjyan@users.noreply.github.com>
2024-01-25 04:45:14 -0800

    Update run_pipeline.py
    "LoS" column dropped

--- run_pipeline.py
+++ run_pipeline.py
@@ -17,7 +17,7 @@ class Pipeline:
         df = pd.read_csv(data_path)
 
         if not test:
-            X = df.drop(columns=['In-hospital_death'])
+            X = df.drop(columns=['In-hospital_death','Length_of_stay'])
             y = df['In-hospital_death']
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
